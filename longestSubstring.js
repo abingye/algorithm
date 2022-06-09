e7e723aa29c8cdd531a4d26ead633505572355e9
wengbingfeng <240720616@qq.com>
2022-06-09 21:21:41 +0800

    chore: 注释

--- longestSubstring.js
+++ longestSubstring.js
@@ -1,3 +1,9 @@
+/**
+ * 思路：遍历字符串并存入set集合中，如果有重复字符就终止当前循环，记录最大字符数并执行下一次，
+ * 每次执行需要将指针右移一个，故需删除set第一个字符
+ * @param s
+ * @returns {number}
+ */
 const lengthOfLongestSubstring = function(s) {
     // 哈希集合，记录每个字符是否出现过
     const occ = new Set();
